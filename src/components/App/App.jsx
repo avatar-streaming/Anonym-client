ff4d4ce22ab3c79ee8ddc42fb3c5452dac4e352a
KimJaedeok <jdcoder27@gmail.com>
2021-05-07 03:20:24 +0900

    setting eslint and scss

---
+++ src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../../styles/styles.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello world</h1>
+    </div>
+  );
+}
+
+export default App;
