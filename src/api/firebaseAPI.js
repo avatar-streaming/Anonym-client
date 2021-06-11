4a91bc1feb1eb195f7e77c68a0dc022e737cfd31
KimJaedeok <jdcoder27@gmail.com>
2021-06-11 16:32:45 +0900

    MODIFY: browerRouter to hashRouter

--- src/api/firebaseAPI.js
+++ src/api/firebaseAPI.js
@@ -6,7 +6,8 @@ const firebaseConfig = {
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
